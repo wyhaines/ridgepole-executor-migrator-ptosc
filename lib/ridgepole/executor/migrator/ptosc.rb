d5cd3b2a1bb3d5aee62c62d3033b37d16a9ac964
Kirk Haines <kirk-haines@cookpad.com>
2018-10-31 21:38:47 -0600

    Cleanups.

--- lib/ridgepole/executor/migrator/ptosc.rb
+++ lib/ridgepole/executor/migrator/ptosc.rb
@@ -1,12 +1,44 @@
+# frozen_string_literal: true
+
 require 'ridgepole/executor/migrator/ptosc/cli'
+require 'shellwords'
+require 'erb'
 
 module Ridgepole
   class Executor
     class Migrator
+      # Handle the invocation of pt-osc.
       class Ptosc
+        ALTER_REGEXP = /^\s*alter[\s\\]+table[\s\\]+([\\\'\"\`\w]+)/i
+
+        attr_reader :raw_sql, :config, :default_host, :default_user,
+                    :default_database, :default_table, :default_password,
+                    :default_charset, :default_recurse, :default_other_flags,
+                    :default_recursion_method, :default_max_load,
+                    :default_critical_load, :default_alter_foreign_keys_method,
+                    :default_chunk_size, :default_sleep, :default_plugin
+
         def initialize
           @cli = Cli.new
+          setup_default_variables
         end
+
+        # rubocop: disable Metrics/MethodLength
+        def setup_default_variables
+          @default_sql = 'select 1'
+          @default_host = '127.0.0.1'
+          @default_charset = 'utf8mb4'
+          @default_recurse = 0
+          @default_recursion_method = 'none'
+          @default_max_load = 'Threads_running=100'
+          @default_critical_load = 'Threads_running=120'
+          @default_alter_foreign_keys_method = 'rebuild_constraints'
+          @default_plugin = ['pt-online-schema-change-fast-rebuild-constraints.pl']
+          @default_chunk_size = 1000
+          @default_sleep = 0.1
+          @default_other_flags = ['--dry-run']
+        end
+        # rubocop: enable Metrics/MethodLength
 
         def parse(config, metaconfig)
           @cli.parse(config, metaconfig)
@@ -14,6 +46,97 @@ module Ridgepole
 
         def config
           @cli.config
+        end
+
+        def strip_quotes(str)
+          str.sub(/^(\s*)[\'\"\`]+/, '\1').sub(/[\'\"\`]+(\s*)$/, '\1')
+        end
+
+        def parse_raw_sql_for_pt_osc
+          @raw_sql =~ ALTER_REGEXP
+          config['table'] = strip_quotes(Regexp.last_match(1))
+          @sql = @raw_sql.gsub(ALTER_REGEXP, '').strip.shellescape
+        end
+
+        def parse_config_equivalencies_for_pt_osc
+          config['user'] = config['username']
+          config['plugin'] = [config['plugin'].to_s.split(/\s*,\s*/)].flatten
+        end
+
+        def run_pt_osc
+          parse_raw_sql_for_pt_osc
+          parse_config_equivalencies_for_pt_osc
+          puts pt_osc_cmdline
+          #          system(pt_osc_cmdline)
+        end
+
+        def default_sql
+          @sql&.empty? ? @default_sql : @sql
+        end
+
+        def default_cmdline
+          <<~ECMD
+            pt-online-schema-change \\
+              --alter <%= sql %> \\
+              --host "<%= host %>" \\
+              -u "<%= user %>" \\
+              D="<%= database %>",t="<%= table %>"="<%= password %>" \\
+              --charset=<%= charset %> \\
+              --recurse=<%= recurse %> \\
+              --recursion-method=<%= recursion_method %> \\
+              --max-load <%= max_load %> \\
+              --critical-load <%= critical_load %> \\
+              --alter-foreign-keys-method <%= alter_foreign_keys_method %> \\
+              <%= plugin.collect {|p| "--plugin #{p}"}.join(' ') %> \\
+              --chunk-size=<%= chunk_size %> \\
+              --sleep=<%= sleep %> \\
+              <%= other_flags&.any? && other_flags.join(' ') %>
+          ECMD
+        end
+
+        def pt_osc_variables
+          %w[sql host user database table password charset recurse
+             recursion_method max_load critical_load alter_foreign_keys_method
+             chunk_size sleep cmdline plugin other_flags]
+        end
+
+        def envname(var)
+          "PTOSC_#{var.upcase}"
+        end
+
+        def env_variable(var)
+          case var
+          when 'plugin'
+            [ENV[envname(var)].to_s.split(/\s*,\s*/)].flatten
+          else
+            ENV[envname(var)]
+          end
+        end
+
+        def pt_osc_cmdline
+          b = binding
+          pt_osc_variables.each do |var|
+            b.local_variable_set(var,
+                                 env_variable(var) ||
+                                 config[var] ||
+                                 __send__(:"default_#{var}"))
+          end
+          cmd_template = ERB.new(b.local_variable_get('cmdline'))
+
+          cmd_template.result(b)
+        end
+
+        def alter_statement?
+          config[:sql] =~ ALTER_REGEXP
+        end
+
+        def will_handle?
+          alter_statement?
+        end
+
+        def do
+          @raw_sql = config[:sql]
+          run_pt_osc
         end
       end
     end
--- lib/ridgepole/executor/migrator/ptosc/cli.rb
+++ lib/ridgepole/executor/migrator/ptosc/cli.rb
@@ -8,6 +8,7 @@ module Ridgepole
   class Executor
     class Migrator
       class Ptosc
+        # rubocop: disable Metrics/ClassLength
         # Parse command line arguments for this CLI.
         class Cli
           Task = Swiftcore::Tasks::Task
@@ -15,133 +16,95 @@ module Ridgepole
           attr_reader :config, :metaconfig
 
           def initialize
-            @config = Config.new
-            @config[:command] = DEFAULT_CMD
-            @metaconfig = Config.new
-          end
-
-          def _opt_bind(opts, call_list)
-            opts.on('-b', '--bind HOST[:PORT}|PATH') do |bind|
-              call_list << Task.new(9000) do
-                if File.exist?(bind)
-                  @config[:socket] = bind
-                else
-                  @config[:host], @config[:port] = bind.split(/:/, 2)
-                end
-              end
-            end
-          end
-
-          def _opt_cmdline(opts, call_list)
-            opts.on('-c', '--command COMMAND') do |command|
-              call_list << Task.new(9000) { @config[:command] = command }
-            end
-          end
-
-          def _opt_database(opts, call_list)
-            opts.on('-d', '--database DATABASE') do |database|
-              call_list << Task.new(9000) { @config[:database] = database }
-            end
-          end
-
-          def _opt_password(opts, call_list)
-            opts.on('-p', '--password PASSWORD') do |password|
-              call_list << Task.new(9000) { @config[:password] = password }
-            end
-          end
-
-          def _opt_user(opts, call_list)
-            opts.on('-u', '--user USERNAME') do |user|
-              call_list << Task.new(9000) { @config[:user] = user }
-            end
           end
 
           def _setup_helptext
             @metaconfig[:helptext] << <<~EHELP
-  The following flags all affect the parameters passed to pt-osc. Unless
-  otherwise noted, all of these will override anything passed via the
-  JSONCONFIG at the end of the command line. Several of these may also inherit
-  configuration parsed from the adapter command line parsing (which will
-  always occur before the migrator command line argument parsing), if they
-  are not provided via the flags below. These will be noted below.
+              The following flags all affect the parameters passed to pt-osc. Unless
+              otherwise noted, all of these will override anything passed via the
+              JSONCONFIG at the end of the command line. Several of these may also inherit
+              configuration parsed from the adapter command line parsing (which will
+              always occur before the migrator command line argument parsing), if they
+              are not provided via the flags below. These will be noted below.
 
-  --migrator-alter SQL:
-    The ALTER statement that will be ran by pt-osc. This overrides anything
-    specified without a flag at the end of the command line (in the SQL
-    position indicated at the top of this help document).
+               --migrator-alter SQL:
+                 The ALTER statement that will be ran by pt-osc. This overrides anything
+                 specified without a flag at the end of the command line (in the SQL
+                 position indicated at the top of this help document).
 
-  --migrator-database DBNAME:
-    Specify the database to operate on.
+               --migrator-database DBNAME:
+                 Specify the database to operate on.
 
-  --migrator-host HOSTNAME:
-    Specify the host that the MySQL database is running on. If not provided,
-    or inherited from the adapter configuration, this defaults to '127.0.0.1'.
+               --migrator-host HOSTNAME:
+                 Specify the host that the MySQL database is running on. If not provided,
+                 or inherited from the adapter configuration, this defaults to '127.0.0.1'.
 
-  --migrator-password PASSWORD:
-    Specify the password to use to authenticate against the database. This will
-    inherit from anything provided to the adapter configuration, but may be
-    specified here if pt-osc is to use different authentication than the
-    adapter does for SQL being executed directly.
+               --migrator-password PASSWORD:
+                 Specify the password to use to authenticate against the database. This
+                 will inherit from anything provided to the adapter configuration, but may
+                 be specified here if pt-osc is to use different authentication than the
+                 adapter does for SQL being executed directly.
 
-  --migrator-table TABLE:
-    Specify the table to operate against. This will be extracted from the
-    SQL being executed if not provided. This parameter would not normally be
-    overridden on the command line.
+               --migrator-table TABLE:
+                 Specify the table to operate against. This will be extracted from the
+                 SQL being executed if not provided. This parameter would not normally be
+                 overridden on the command line.
 
-  --migrator-user USERNAME:
-    Specify the username to use to authenticate against the database. This will
-    inherit from anything provided to the adapter configuration, but may be
-    specified here if pt-osc is to use different authentication  than the
-    adapter does for SQL being executed directly.
+               --migrator-user USERNAME:
+                 Specify the username to use to authenticate against the database. This
+                 will inherit from anything provided to the adapter configuration, but may
+                 be specified here if pt-osc is to use different authentication  than the
+                 adapter does for SQL being executed directly.
 
-  --migrator-alter-foreign-keys-method METHOD:
-    Specify the method that pt-osc will use to handle foreign keys. If not
-    provided, this defaults to 'rebuild_constraints'.
+               --migrator-alter-foreign-keys-method METHOD:
+                 Specify the method that pt-osc will use to handle foreign keys. If not
+                 provided, this defaults to 'rebuild_constraints'.
 
-  --migrator-charset CHARSET:
-    Specify the character set to use. If not provided, this defaults to
-    'utf8mb4'.
+               --migrator-charset CHARSET:
+                 Specify the character set to use. If not provided, this defaults to
+                 'utf8mb4'.
 
-  --migrator-chunk-size SIZE:
-    Specify the chunk size for pt-osc operations. If not provided, this
-    defaults to '1000'.
+               --migrator-chunk-size SIZE:
+                 Specify the chunk size for pt-osc operations. If not provided, this
+                 defaults to '1000'.
 
-  --migrator-critical-load LOAD:
-    Specify the critical load parameter for pt-osc. If not provided, this
-    defaults to 'Threads_running=120'.
+               --migrator-critical-load LOAD:
+                 Specify the critical load parameter for pt-osc. If not provided, this
+                 defaults to 'Threads_running=120'.
 
-  --migrator-dry-run:
-    Tell pt-osc to do perform a dry run on the requested ALTER.
+               --migrator-dry-run:
+                 Tell pt-osc to do perform a dry run on the requested ALTER.
 
-  --migrator-execute
-    Tell pt-osc to apply the changes being requested. This is the default.
+               --migrator-execute
+                 Tell pt-osc to apply the changes being requested. This is the default.
 
-  --migrator-max-load LOAD:
-    Specify the max load parameter for pt-osc. If the load exceeds what is
-    provided, pt-osc will pause to allow it to fall. If this is not provided,
-    it will default to 'Threads_running=100'.
+               --migrator-max-load LOAD:
+                 Specify the max load parameter for pt-osc. If the load exceeds what is
+                 provided, pt-osc will pause to allow it to fall. If this is not provided,
+                 it will default to 'Threads_running=100'.
 
-  --migrator-plugin PLUGINNAME:
-    Specify an external plugin for pt-osc to run.
+               --migrator-plugin PLUGINNAME:
+                 Specify an external plugin for pt-osc to run.
 
-  --migrator-recurse DEPTH:
-    Specify the recursion depth for pt-osc when discovering replicas. This
-    works with the recursion-method, and will default to '0'.
+               --migrator-recurse DEPTH:
+                 Specify the recursion depth for pt-osc when discovering replicas. This
+                 works with the recursion-method, and will default to '0'.
 
-  --migrator-recursion-method METHOD:
-    Specify the recursion method to use to discover replicas. This will
-    default to 'none'.
+               --migrator-recursion-method METHOD:
+                 Specify the recursion method to use to discover replicas. This will
+                 default to 'none'.
 
-  --migrator-sleep TIME:
-    Specify the amount of time to sleep between processing of chunks. A sleep
-    period helps to ensure that the database does not get overwhelmed with
-    word related to performing the ALTER change.
+               --migrator-sleep TIME:
+                 Specify the amount of time to sleep between processing of chunks. A sleep
+                 period helps to ensure that the database does not get overwhelmed with
+                 word related to performing the ALTER change.
+
             EHELP
           end
 
           def _opt_alter(opts, call_list)
             opts.on('--migrator-alter') do |sql|
-              call_list << Task.new(9000) { @config[:alter] = sql }
+              call_list << Task.new(9000) { @config[:sql] = sql }
             end
           end
 
@@ -195,7 +158,9 @@ module Ridgepole
 
           def _opt_critial_load(opts, call_list)
             opts.on('--migrator-critical-load') do |critical_load|
-              call_list << Task.new(9000) { @config[:critical_load] = critical_load }
+              call_list << Task.new(9000) do
+                @config[:critical_load] = critical_load
+              end
             end
           end
 
@@ -231,7 +196,9 @@ module Ridgepole
 
           def _opt_recursion_method(opts, call_list)
             opts.on('--migrator-recursion-method') do |recursion_method|
-              call_list << Task.new(9000) { @config[:recursion_method] = recursion_method }
+              call_list << Task.new(9000) do
+                @config[:recursion_method] = recursion_method
+              end
             end
           end
 
@@ -242,9 +209,9 @@ module Ridgepole
           end
 
           def _handle_options(opts, call_list)
-            %q{alter database host password table user fkmethod charset
+            %(alter database host password table user fkmethod charset
                chunk_size critical_load dry_run execute max_load plugin
-               recurse recursion_method sleep}.each do |m|
+               recurse recursion_method sleep).each do |m|
               __send__(:"_opt_#{m}", opts, call_list)
             end
           end
@@ -265,8 +232,9 @@ module Ridgepole
           end
 
           def parse(config, metaconfig)
-            @config.merge!(config)
-            @metaconfig.merge!(metaconfig)
+            @config = config
+            @config[:command] ||= DEFAULT_CMD
+            @metaconfig = metaconfig
             call_list = Swiftcore::Tasks::TaskList.new
             _setup_helptext
 
@@ -278,6 +246,7 @@ module Ridgepole
             call_list.run
           end
         end
+        # rubocop: enable Metrics/ClassLength
       end
     end
   end
